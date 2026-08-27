9da350001d9b752a6b00472b59cac5a2871443b4
schoolidol <56410849+schoolidol@users.noreply.github.com>
2026-08-27 16:12:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const DEFAULT_FALLBACK_MODEL = 'meta/llama-3.1-405b-instruct';
 
 const RETRYABLE_STATUSES = [503, 429, 410];
 
-const REQUEST_TIMEOUT_MS = 50000;
+const REQUEST_TIMEOUT_MS = 120000;
 
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'nvidia/llama-3.1-nemotron-ultra-253b-v1',
